42330515fdde1dda94e9146237b18a371eee8dbb
TayMcKenzieSA <taylorocarroll@gmail.com>
2023-05-08 23:54:37 +1200

    Update NoIdleCam.lua

--- client/NoIdleCam.lua
+++ client/NoIdleCam.lua
@@ -2,7 +2,7 @@ RegisterCommand('idlecamoff', function() -- help2 31, 167, 9
   TriggerEvent('chat:addMessage', {
     color = {227,8,0},
     multiline = true,
-    args = {'[COMMANDS]', 'Idle Cam Is Now Off'}
+    args = {'[RPEmotes]', 'Idle Cam Is Now Off'}
   })
   DisableIdleCamera(true)
   SetPedCanPlayAmbientAnims(playerPed, false)
@@ -13,7 +13,7 @@ RegisterCommand('idlecamon', function() -- help2 31, 167, 9
   TriggerEvent('chat:addMessage', {
     color = {31,167,9},
     multiline = true,
-    args = {'[COMMANDS]', 'Idle Cam Is Now On'}
+    args = {'[RPEmotes]', 'Idle Cam Is Now On'}
   })
   DisableIdleCamera(false)
   SetPedCanPlayAmbientAnims(playerPed, true)
