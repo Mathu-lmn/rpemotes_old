28002bc7d8ae6859bf80bdc2958072048096455a
TayMcKenzieSA <taylorocarroll@gmail.com>
2023-08-23 14:58:38 +1200

    Fix Moods For ESX & QbCore PlayerSpawned

--- client/Expressions.lua
+++ client/Expressions.lua
@@ -37,14 +37,36 @@ if Config.ExpressionsEnabled then
     TriggerEvent('chat:addSuggestion', '/mood', 'Set your current mood/expression.', { { name = "expression", help = "/moods for a list of valid moods" } })
     TriggerEvent('chat:addSuggestion', '/moods', 'List available walking moods/expressions.')
 
-    -- Persistent Expressions
-    if Config.PersistentExpression then
-        AddEventHandler('playerSpawned', function()
-            local expression = GetResourceKvpString("expression")
-            if expression ~= nil then
-                Wait(2500) -- Delay, to ensure the player ped has loaded in
-                SetPlayerPedExpression(expression, false)
-            end
-        end)
-    end
+
+    -- Load the expression once the player has spawned. Standalone, QBCore and ESX --
+if Config.PersistentExpression then
+    AddEventHandler('playerSpawned', function()
+        local expression = GetResourceKvpString("expression")
+        if expression ~= nil then
+            Wait(2500) -- Delay, to ensure the player ped has loaded in
+            SetPlayerPedExpression(expression, false)
+        end
+    end)
+
+    RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
+    AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+        Citizen.Wait(5000)
+        local expression = GetResourceKvpString("expression")
+        if expression ~= nil then
+            Wait(2500) -- Delay, to ensure the player ped has loaded in
+            SetPlayerPedExpression(expression, false)
+        end
+    end)
+
+    RegisterNetEvent('esx:playerLoaded')
+    AddEventHandler('esx:playerLoaded', function()
+        Citizen.Wait(5000)
+        local expression = GetResourceKvpString("expression")
+        if expression ~= nil then
+            Wait(2500) -- Delay, to ensure the player ped has loaded in
+            SetPlayerPedExpression(expression, false)
+        end
+    end)
+end
+
 end
