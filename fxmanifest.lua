6522fcc67a19b67f62c6e239b8efd0be213d74e0
TayMcKenzieSA <taylorocarroll@gmail.com>
2023-10-19 02:15:30 +1300

    1.3.7 Version Change

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,8 +5,8 @@
 fx_version 'cerulean'
 game 'gta5'
 authors { 'TayMcKenzieNZ', 'MadsL', 'Mathu_lmn', 'Community' }
-description 'RPEmotes V1.3.6'
-version '1.3.6'
+description 'RPEmotes V1.3.7'
+version '1.3.7'
 lua54 'yes'
 
 dependencies {
